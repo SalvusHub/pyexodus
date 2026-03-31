ae0e77d3832279181175c0bece386f09aa42a6db
Christian Boehm <christian.boehm@mondaic.com>
2026-03-31 20:16:15 +0200

    Code review.

--- pyexodus/__init__.py
+++ pyexodus/__init__.py
@@ -3,12 +3,12 @@
 """
 :copyright:
     Lion Krischer (lion.krischer@mondaic.com), 2016-2026
+    Mondaic Ltd. (info@mondaic.com), 2020-2026
 :license:
     MIT License
 """
+from importlib.metadata import version as _get_version
+
 from .core import exodus  # NOQA
 
-try:
-    from ._version import version as __version__
-except ImportError:
-    __version__ = "0.0.0"
+__version__ = _get_version("pyexodus")
